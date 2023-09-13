587f6b91cb9e2edde13040b83d98f52ddd50e1fe
leoliyou <liyouleoleo@gmail.com>
2023-09-14 00:40:30 +0100

    todo1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [todos,setTodos]=useState([
+    'read','write','stay', 'reply'
+  ])
+
+  const [todo, setTodo] = useState('');
+
+  const handleClick = () => {
+    if (!todo){  return;}
+
+    const newTodos = todos.concat([todo])
+    setTodos(newTodos)
+    setTodo("");
+
+  }
+
+  const handleChange = (e) => {
+    const todo = e.target.value
+    setTodo(todo)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <div>
+        <input type="text" onChange={ handleChange} value={todo} />
+        <button onClick={ handleClick}>add</button>
+      </div>
+
+      {/* <ul>
+        <li>read </li>
+        <li>write</li>
+        <li>listen</li>
+      </ul> */}
+
+      <ul>
+        {todos.map((todo) => {
+          return <li>{ todo}</li>
+        })}
+      </ul>
     </div>
   );
 }
